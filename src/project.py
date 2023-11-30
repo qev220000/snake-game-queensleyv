8c649d7e5a36dbd639ee8b39adc02b11e5fc2977
Queensley Valenzuela <qev220000@utdallas.edu>
2023-11-30 16:06:21 -0600

    making basic surface and fruit

---
+++ src/project.py
@@ -0,0 +1,30 @@
+import pygame, sys
+from pygame.math import Vector2
+
+class FRUIT:
+    def __init__(self):
+        self.x = 5
+        self.y = 4
+        self.pos = Vector2(self.x,self.y)
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(self.pos.x, self.pos.y, cell_size,cell_size)
+        pygame.draw.rect(screen,(126,166,114), fruit_rect)
+
+pygame.init()
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_size*cell_number, cell_size*cell_number))
+clock = pygame.time.Clock()
+
+fruit = FRUIT()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill((175,215,70))
+    fruit.draw_fruit()
+    pygame.display.update()
+    clock.tick(60)
